be7da41ce7d2bcd422771fa2012ff724356bec6a
white-wolf <jean-christophe.white@ovh.fr>
2022-02-26 18:47:04 +0100

    modifié :         chapitre-2.lua 	modifié :         main.lua

--- Jeux/chapitre-2.lua
+++ Jeux/chapitre-2.lua
@@ -5,4 +5,11 @@ chapitre02 = {}
 chapitre02[1] = {}
 chapitre02[1][1] = {}
 
-chapitre02[1][1][1] = "Vous vous réveillez l'esprit embrumé, la tête lourde et autour de vous tous est flou. Vous mettez quel minutes à retrouver la vision et vous vous appercevez que vous est dans une infirmerie. Un personne s'approche de vous et vous demande si vous allez bien, à ce moment là tous est flou dans votre esprit et la première chose que vous demandez à l'infirmière à côté de vous est le lieu ou vous vous trouvez. Elle vous répond dans l'infirmerie du vaiseau qui va en direction de la citadelle.\n - Vous est confuse mademoiselle reposer vous...\n - Je ne me souviens de rien, DE RIEN ! Vous entendez !\n - Calmez vous mademoiselle, je vous en pris calmer vous !\n [vous vous calmez-(1)] ou [vous arrachez votre perfusion-(2)]\n"
+chapitre02[1][1][1] = "Vous vous réveillez l'esprit embrumé, la tête lourde et autour de vous tous est flou. Vous mettez quel minutes à retrouver la vision et vous vous appercevez que vous est dans une infirmerie. Un personne s'approche de vous et vous demande si vous allez bien, à ce moment là tous est flou dans votre esprit et la première chose que vous demandez à l'infirmière à côté de vous est le lieu ou vous vous trouvez. Elle vous répond dans l'infirmerie du vaiseau qui va en direction de la citadelle.\n - Vous est confuse mademoiselle reposer vous...\n - Je ne me souviens de rien, DE RIEN ! Vous entendez !\n - Calmez vous mademoiselle, je vous en pris calmer vous !\n\n[Vous vous calmez] - (1) ou [Vous arrachez votre perfusion] - (2)"
+
+chapitre02[1][1][2] = "Vous vous calmez et...\n"
+
+chapitre02[1][1][3] = "Vous arrachez votre perfusion et...\n"
+
+
+
--- Jeux/main.lua
+++ Jeux/main.lua
@@ -1,14 +1,24 @@
 require("chapitre-1")
 require("chapitre-2")
 
+choix = 0
+
 for n = 1, #chapitre01 do
 	print(n, chapitre01[n])
 end
 
-for n = 1, #chapitre02 do
-	for l = 1, #chapitre02[n] do
-		for c = 1, #chapitre02[n][l] do
-			print(n, l, c, chapitre02[n][l][c])
-		end
-	end
+print(chapitre02[1][1][1])
+
+print("[Vous vous calmez] - (1) ou [Vous arrachez votre perfusion] - (2)\n")
+choix = io.read()
+print("Ok ! Vous avez fait le choix : "..choix)
+
+allerA = tonumber(choix)
+
+if allerA == 1 then
+	print(chapitre02[1][1][2])
+elseif allerA == 2 then
+	print(chapitre02[1][1][3])
 end
+
+
