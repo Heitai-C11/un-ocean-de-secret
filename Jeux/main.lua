f7400ae2ef123095990ff67ea214c84a90d3521b
white-wolf <jean-christophe.white@ovh.fr>
2022-02-26 17:33:37 +0100

    modifié :         Jeux/chapitre-1.lua 	nouveau fichier : Jeux/chapitre-2.lua 	modifié :         Jeux/main.lua

--- Jeux/main.lua
+++ Jeux/main.lua
@@ -1,5 +1,14 @@
 require("chapitre-1")
+require("chapitre-2")
 
-for n = 1, #histoire do
-	print(histoire[n])
+for n = 1, #chapitre01 do
+	print(n, chapitre01[n])
+end
+
+for n = 1, #chapitre02 do
+	for l = 1, #chapitre02[n] do
+		for c = 1, #chapitre02[n][l] do
+			print(n, l, c, chapitre02[n][l][c])
+		end
+	end
 end
